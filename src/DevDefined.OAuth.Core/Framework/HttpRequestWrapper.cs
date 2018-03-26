c657f85ed05db69aef7e96143837fdb765a7dd65
Chris Ballance <chris@ballance.it>
2018-03-25 21:29:33 -0400

    port to .net core - HttpWrapper may need some work

---
+++ src/DevDefined.OAuth.Core/Framework/HttpRequestWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+
+namespace DevDefined.OAuth.Core.Framework
+{
+    public class HttpRequestWrapper : HttpRequestBase
+    {
+        public HttpRequestWrapper(HttpRequest httpRequest)
+        {
+        }
+    }
+}
